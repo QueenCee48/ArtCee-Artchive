0bea4321758e0866afc1382453565013ce66e166
Ciera <177058032+QueenCee48@users.noreply.github.com>
2026-03-30 21:41:41 -0400

    complete branding room

--- Assets/Scripts/ExhibitController.cs
+++ Assets/Scripts/ExhibitController.cs
@@ -99,76 +99,112 @@ public class ExhibitController : MonoBehaviour
                 storyTxt = "\n\n<i>My recreation of a 90's vibe graphic I found on Pinterest.</i>";
                 break;
             case "EDCSV":
-                aboutTxt = "\n              <b><i>Every Dollar Counts</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Every Dolla Counts</i></b> \n\n\n<b>Date:</b>  <i>November 2020</i> \n\n<b>Tools:</b>  <i>Adobe Illustrator, Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>My illustrative interpretation of a character based on a roll of money for a small brand known as EDC.</i>";
                 break;
             case "FRVR22SV":
-                aboutTxt = "\n              <b><i>Forever 22</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Forever 22</i></b> \n\n\n<b>Date:</b>  <i>January 2021</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>I put this together with the given photo of the rapper and a list of words they wanted in the background.</i>";
                 break;
             case "FastTimesSV":
-                aboutTxt = "\n              <b><i>Fast Times</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Fast Times</i></b> \n\n\n<b>Date:</b>  <i>January 2021</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>An illustration done based on a picture of the artist and the request for beautiful women in the background.</i>";
                 break;
             case "TheDonSV":
-                aboutTxt = "\n              <b><i>The Don</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>The Don</i></b> \n\n\n<b>Date:</b>  <i>October 2021</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>An illustrative cover with Black Caesar as the provided inspiration.</i>";
                 break;
             case "MOMMSV":
-                aboutTxt = "\n              <b><i>Money On My Mind</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Money On My Mind</i></b> \n\n\n<b>Date:</b>  <i>December 2020</i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>A bright playful play on the phrase 'money on my mind'.</i>";
                 break;
             case "ArchangelSV":
-                aboutTxt = "\n              <b><i>Archangel</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Archangel</i></b> \n\n\n<b>Date:</b>  <i>September 2024</i> \n\n<b>Tools:</b>  <i>Procreate, Canva</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>The dark side of a two part angel series.</i>";
                 break;
             case "EarthAngelSV":
-                aboutTxt = "\n              <b><i>Earth Angel</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Earth Angel</i></b> \n\n\n<b>Date:</b>  <i>July 2024</i> \n\n<b>Tools:</b>  <i>Procreate, Canva</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>The light side of a two part angel series.</i>";
                 break;
             case "RightTimeSV":
-                aboutTxt = "\n              <b><i>Right Time</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Right Time</i></b> \n\n\n<b>Date:</b>  <i>April 2021</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>An illustrative work based on an image of the artist and their car with the request for a cityscape in the background.</i>";
                 break;
             case "LiveLifeSV":
-                aboutTxt = "\n              <b><i>Live That Life</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Live That Life</i></b> \n\n\n<b>Date:</b>  <i>October 2021</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>My interpretation of a party scene including a cameo of the artist.</i>";
                 break;
             case "ElegEleSV":
-                aboutTxt = "\n              <b><i>Elegant Elephant</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Elegant Elephant</i></b> \n\n\n<b>Date:</b>  <i>June 2020</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>An illustration of an elephant with a lotus headress and smoke rings for a small hookah business.</i>";
                 break;
             case "CurlyQueenSV":
-                aboutTxt = "\n              <b><i>Curly Queen</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Curly Queen</i></b> \n\n\n<b>Date:</b>  <i>October 2020</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>A cartoonized version of a portrait.</i>";
                 break;
             case "AngelSV":
-                aboutTxt = "\n              <b><i>Angel</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Angel</i></b> \n\n\n<b>Date:</b>  <i>June 2023</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>An angel created from a photo provided.</i>";
                 break;
             case "WisdomSV":
-                aboutTxt = "\n              <b><i>Wisdom</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Wisdom</i></b> \n\n\n<b>Date:</b>  <i>March 2021</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>An illustration of sloths to be printed on a blanket.</i>";
                 break;
             case "LineOLoveSV":
-                aboutTxt = "\n              <b><i>Lines of Love</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Lines of Love</i></b> \n\n\n<b>Date:</b>  <i>February 2024</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>An outline of a couple's photo to be embroidered on a hoodie for Valentine's day.</i>";
                 break;
             case "RoleModelsSV":
-                aboutTxt = "\n              <b><i>Role Models</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Role Models</i></b> \n\n\n<b>Date:</b>  <i>September 2023</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>Cartoonized versions of two sports players.</i>";
                 break;
             case "TakeoffSV":
-                aboutTxt = "\n              <b><i>Takeoff</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Takeoff</i></b> \n\n\n<b>Date:</b>  <i>February 2024</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>An illustration of the clients car for a t-shirt design.</i>";
                 break;
             case "PinkLemonsSV":
-                aboutTxt = "\n              <b><i>Pink Lemons</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>Pink Lemonade</i></b> \n\n\n<b>Date:</b>  <i>March 2022</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>An illustration to demonstrate friendship for a small embroidery company named Pink Lemons.</i>";
                 break;
             case "ComebackSV":
-                aboutTxt = "\n              <b><i>The Comeback</i></b> \n\n\n<b>Date:</b>  <i></i> \n\n<b>Tools:</b>  <i></i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
-                storyTxt = "\n\n<i></i>";
+                aboutTxt = "\n              <b><i>The Comeback</i></b> \n\n\n<b>Date:</b>  <i>September 2024</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>A graphic for breast cancer awareness t-shirts.</i>";
+                break;
+            case "EDSV":
+                aboutTxt = "\n              <b><i>Evergreen Dreams</i></b> \n\n\n<b>Date:</b>  <i>January 2025</i> \n\n<b>Tools:</b>  <i>Affinity Designer, Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>A logo for Evergreen Dreams Floral Studio. Features flowers around the main shape.</i>";
+                break;
+            case "PLSV":
+                aboutTxt = "\n              <b><i>Pink Lemons</i></b> \n\n\n<b>Date:</b>  <i>December 2020</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>A logo for Pink Lemons Embroidery. Features a needle and thread around the 'PL'.</i>";
+                break;
+            case "IHUSV":
+                aboutTxt = "\n              <b><i>iHelpU</i></b> \n\n\n<b>Date:</b>  <i>July 2022</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>A logo a small company called iHelpU. Features a sloth hugging a shopping bag.</i>";
+                break;
+            case "TESV":
+                aboutTxt = "\n              <b><i>Tuk Entertainment</i></b> \n\n\n<b>Date:</b>  <i>May 2024</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>A logo for a company called Tuk Entertainment.</i>";
+                break;
+            case "WOSV":
+                aboutTxt = "\n              <b><i>Walden's Outdoors</i></b> \n\n\n<b>Date:</b>  <i>September 2024</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>A logo for a landscaping company called Walden's Outdoors.</i>";
+                break;
+            case "BESV":
+                aboutTxt = "\n              <b><i>Baucham Elite</i></b> \n\n\n<b>Date:</b>  <i>March 2024</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>A logo for a small sports store called Baucham Elite.</i>";
+                break;
+            case "AC2SV":
+                aboutTxt = "\n              <b><i>ArtCee V.2</i></b> \n\n\n<b>Date:</b>  <i>February 2024</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>A logo created for my Digital Design class for my artist name ArtCee.</i>";
+                break;
+            case "ACSV":
+                aboutTxt = "\n              <b><i>Piece of ArtCee</i></b> \n\n\n<b>Date:</b>  <i>January 2022</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>A puzzle piece logo created from the shape of 3D letters A and C for my artist name ArtCee.</i>";
+                break;
+            case "LCCSV":
+                aboutTxt = "\n              <b><i>Larey Carl Choice</i></b> \n\n\n<b>Date:</b>  <i>March 2026</i> \n\n<b>Tools:</b>  <i>Procreate</i> \n\n<b>Artist:</b>  <i>Ciera Baucham</i> \n\n<b>Medium:</b>  <i>Digital Art</i>";
+                storyTxt = "\n\n<i>A logo for a small family owned business called Larey Carl Choice.</i>";
                 break;
         }
         detailViewCntrlr.OpenDetailView(detailViewSprite, aboutTxt, storyTxt);
