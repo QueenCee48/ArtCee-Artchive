521fe833fdd4356c4e6b2431e2fc934e60ac0ded
Ciera <177058032+QueenCee48@users.noreply.github.com>
2026-05-04 13:36:11 -0400

    add sound fx

---
+++ Assets/Scripts/SoundFXController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFXController : MonoBehaviour
+{
+    AudioSource audioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            audioSrc.Play();
+        }
+    }
+}
